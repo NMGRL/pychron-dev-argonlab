60709cfbe031c6272fa8cb9f97dd47cd28e93a14
Alex Fulop <Alex@AlexFMBPRO.local>
2026-08-27 16:25:58 -0600

    fix formatting for smoketest

--- pychron/core/tests/ci_pipeline_smoke_test.py
+++ pychron/core/tests/ci_pipeline_smoke_test.py
@@ -1,5 +1,5 @@
-""""Throwaway file used to verify the CI pipeline end to end."""
+""" "Throwaway file used to verify the CI pipeline end to end."""
 
 
 def ci_smoke_check() -> bool:
-    return True 
+    return True
